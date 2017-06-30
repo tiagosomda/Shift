47a1b41ab1571f73bcb27587f705634837c77199
tiagosomda <tsomda@gmail.com>
2017-06-30 10:47:58 -0700

    added test page

--- src/Assets/Scripts/General/DeathTrigger.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class DeathTrigger: MonoBehaviour {
-
-	void OnTriggerEnter( Collider other ) {
-		other.gameObject.SendMessage("OnDeath", SendMessageOptions.DontRequireReceiver);
-	}
-}
